6f08d3ca2e8140ac9f203838b02991d4cf9a32a6
KEI_IdleSystem <appopiss1840@gmail.com>
2021-03-09 11:05:47 +0900

    Upgradeのeditor拡張中...

--- LibraryEditor/Assets/Editor/Upgrade_Editor.cs
+++ LibraryEditor/Assets/Editor/Upgrade_Editor.cs
@@ -18,6 +18,15 @@ public class Upgrade_Editor : Editor
         serializedObject.Update();
 
         EditorGUILayout.LabelField("コスト設定", bold);
+        
+        //リソースの種類を指定させます。
+        EditorGUILayout.LabelField("コストとして使用するリソースの数と種類を指定してください");
+        var resourseNames = serializedObject.FindProperty("resourseNames");
+        EditorGUILayout.PropertyField(resourseNames, true);
+        if (resourseNames.arraySize == 0)
+            return;
+
+        //コストの計算方法を指定させます。
         var costkind = serializedObject.FindProperty("costkind");
         costkind.enumValueIndex = EditorGUILayout.Popup(
             "コストの計算方法",
@@ -25,13 +34,46 @@ public class Upgrade_Editor : Editor
             new string[] { "線形関数", "指数関数" }
             );
 
-        if(costkind.enumValueIndex == (int)CostKind.linear)
+        if (costkind.enumValueIndex == (int)CostKind.linear)
         {
             var initialValue = serializedObject.FindProperty("linear_initialValue");
             var steep = serializedObject.FindProperty("linear_steep");
             initialValue.doubleValue = EditorGUILayout.DoubleField("コストの初期値",initialValue.doubleValue);
             steep.doubleValue = EditorGUILayout.DoubleField("コストの傾き",steep.doubleValue);
         }
+
+        EditorGUILayout.Space();
+
+        //効果を設定させます。
+        EditorGUILayout.LabelField("効果設定", bold);
+        var effectKind = serializedObject.FindProperty("effectKind");
+        effectKind.enumValueIndex = EditorGUILayout.Popup(
+            "アップグレードの効果",
+            effectKind.enumValueIndex,
+            new string[] { "Number型を強化します。", "Cal型を強化します。", "クリックを強化します。", "自動生産を強化します。" }
+            );
+
+        //強化対象
+        //cal以外を選んでいれば
+        if(effectKind.enumValueIndex == (int)EffectKind.Cal)
+        {
+            var targetCal = serializedObject.FindProperty("targetCal");
+            EditorGUILayout.PropertyField(targetCal, true);
+        }
+        else
+        {
+            var targetNumber = serializedObject.FindProperty("targetNumber");
+            EditorGUILayout.PropertyField(targetNumber, true);
+        }
+
+        //加算か乗算か？
+        var calway = serializedObject.FindProperty("calway");
+        calway.enumValueIndex = EditorGUILayout.Popup(
+            "加算か乗算か？",
+            calway.enumValueIndex,
+            new string[] {"加算", "乗算"}
+            );
+
 
         //　シリアライズオブジェクトのプロパティの変更を更新
         serializedObject.ApplyModifiedProperties();
--- LibraryEditor/Assets/MonoScript/Upgrade/Upgrade_Mono.cs
+++ LibraryEditor/Assets/MonoScript/Upgrade/Upgrade_Mono.cs
@@ -2,13 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EffectKind
+{
+    Number,
+    Cal,
+    Click,
+    Produce
+}
+public enum CalculateWay
+{
+    additive,
+    multiplicative
+}
 public class Upgrade_Mono : MonoBehaviour, ILevel
 {
     public long level { get; set; }
     [SerializeField]
     CostKind costkind;
     [SerializeField]
+    NumbersName[] resourseNames;
+    [SerializeField]
     double linear_initialValue, linear_steep;
+    [SerializeField]
+    CalculateWay calway;
+    [SerializeField]
+    NumbersName targetNumber;
+    [SerializeField]
+    CalsName targetCal;
+
+    [SerializeField]
+    EffectKind effectKind;
     // アップグレードを作成します。
     void Awake()
     {
