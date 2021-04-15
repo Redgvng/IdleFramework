71c4b65e9131e9fd651a4ce4491ec3c03a3727a8
Appopiss <appopiss1840@gmail.com>
2021-04-15 20:48:17 +0900

    指数もOK。今からIEHに移植します

--- LibraryEditor/Assets/Script/Upgrade/Cost/Cost.cs
+++ LibraryEditor/Assets/Script/Upgrade/Cost/Cost.cs
@@ -7,6 +7,8 @@ namespace IdleLibrary.Upgrade
     public interface ICost
     {
         Cal Cost { get; }
+        long LevelAtMaxCost(NUMBER number);
+        double MaxCost(NUMBER number);
     }
 
     public class MaxCost
@@ -35,19 +37,30 @@ namespace IdleLibrary.Upgrade
             Cost = new CalDL((level) => initialValue + level * steep, level);
         }
 
-        public (long level, double cost) MaxCostInfo(NUMBER number)
+        public long LevelAtMaxCost(NUMBER number)
         {
             double n = number.Number;
             double a = initialValue;
             double b = steep;
             long level = this.level.level;
             //現在のレベルを基準にして...
-            double TotalCost(long maxLevel) => -a * level + a * maxLevel - b * Math.Pow(level, 2) / 2 +
-                b * level / 2 + b * Math.Pow(maxLevel, 2) / 2 - b * maxLevel / 2;
+
             long SolveX() => b != 0 ? (long)((Math.Sqrt(4 * Math.Pow(a, 2) + 4 * a * b * (2 * level - 1) + b *
                 (b * Math.Pow(1 - 2 * level, 2) + 8 * n)) - 2 * a + b) / 2 / b) : (long)(n / a + level);
 
-            return (SolveX(), TotalCost(SolveX()));
+            return SolveX();
+        }
+
+        public double MaxCost(NUMBER number)
+        {
+            double n = number.Number;
+            double a = initialValue;
+            double b = steep;
+            long level = this.level.level;
+            double TotalCost(long maxLevel) => -a * level + a * maxLevel - b * Math.Pow(level, 2) / 2 +
+            b * level / 2 + b * Math.Pow(maxLevel, 2) / 2 - b * maxLevel / 2;
+
+            return TotalCost(LevelAtMaxCost(number));
         }
     }
 
@@ -59,11 +72,40 @@ namespace IdleLibrary.Upgrade
         public Cal Cost { get; }
         public ExponentialCost(double initialValue, double factor, ILevel level)
         {
+            if(factor == 1)
+            {
+                Debug.LogError("1入れないで〜");
+            }else if(factor < 1)
+            {
+                Debug.LogError("1より大きい値入れて〜");
+            }
             this.initialValue = initialValue;
             this.factor = factor;
             this.level = level;
             Cost = new Cal(initialValue);
             Cost.multiplier.AddMultiplicativeMultiplier(() => Math.Pow(factor, level.level));
+        }
+        public long LevelAtMaxCost(NUMBER number)
+        {
+            double n = number.Number;
+            double a = initialValue;
+            double b = factor;
+            long level = this.level.level;
+
+            long SolveX() => (long)(Math.Log((b - 1) * n / a + Math.Pow(b, level)) / Math.Log(b));
+
+            return SolveX();
+        }
+
+        public double MaxCost(NUMBER number)
+        {
+            double n = number.Number;
+            double a = initialValue;
+            double b = factor;
+            long level = this.level.level;
+            double TotalCost(long maxLevel) => a * (Math.Pow(b, maxLevel) - Math.Pow(b, level)) / (b - 1);
+
+            return TotalCost(LevelAtMaxCost(number));
         }
     }
 
--- LibraryEditor/Assets/Tests/EditMode/Editor/Upgrade/LinearCostTest.cs
+++ LibraryEditor/Assets/Tests/EditMode/Editor/Upgrade/LinearCostTest.cs
@@ -5,7 +5,7 @@ using UnityEngine.TestTools;
 using IdleLibrary.Upgrade;
 using IdleLibrary;
 
-namespace Tests
+namespace Tests.Cost
 {
     public class LinearCostTest
     {
@@ -16,9 +16,10 @@ namespace Tests
             LinearCost cost = new LinearCost(1, 2, level);
             NUMBER gold = new NUMBER(9);
             //3個かえて、goldは1残るはず
-            var unko = cost.MaxCostInfo(gold);
-            Assert.AreEqual(3, unko.level);
-            Assert.AreEqual(9, unko.cost);
+            var l = cost.LevelAtMaxCost(gold);
+            var c = cost.MaxCost(gold);
+            Assert.AreEqual(3, l);
+            Assert.AreEqual(9, c);
         }
         [Test]
         public void CanSolveLevelAndCostAtMax2()
@@ -28,9 +29,10 @@ namespace Tests
             LinearCost cost = new LinearCost(1, 2, level);
             NUMBER gold = new NUMBER(13);
             //2個かえて、goldは1残るはず
-            var unko = cost.MaxCostInfo(gold);
-            Assert.AreEqual(4, unko.level);
-            Assert.AreEqual(12, unko.cost);
+            var l = cost.LevelAtMaxCost(gold);
+            var c = cost.MaxCost(gold);
+            Assert.AreEqual(4, l);
+            Assert.AreEqual(12, c);
         }
         [Test]
         public void CanSolveLevelAndCostAtMax3()
@@ -40,10 +42,27 @@ namespace Tests
             LinearCost cost = new LinearCost(2, 0, level);
             NUMBER gold = new NUMBER(100);
             //2個かえて、goldは1残るはず
-            var unko = cost.MaxCostInfo(gold);
+            var l = cost.LevelAtMaxCost(gold);
+            var c = cost.MaxCost(gold);
             //50回アップグレードできるはず
-            Assert.AreEqual(50, unko.level);
-            Assert.AreEqual(100, unko.cost);
+            Assert.AreEqual(50, l);
+            Assert.AreEqual(100, c);
+        }
+    }
+
+    public class ExponentialCostTest
+    {
+        [Test]
+        public void CanSolveLevelAndCostAtMax()
+        {
+            ILevel level = new MockLevel();
+            ExponentialCost cost = new ExponentialCost(1, 1.5, level);
+            NUMBER gold = new NUMBER(5);
+            //3個かえて、goldは1残るはず
+            var l = cost.LevelAtMaxCost(gold);
+            var c = cost.MaxCost(gold);
+            Assert.AreEqual(3, l);
+            Assert.AreEqual(4.75, c);
         }
     }
 }
