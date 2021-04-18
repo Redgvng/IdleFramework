16a46ebab37b5c94d826350a0e85ef74983f6db7
hidenori16 <hidenori@keio.jp>
2021-04-18 16:48:55 +0900

    mainグチャり

--- LibraryEditor/Assets/Script/Mobile/BattleLog/LOGTEXT.cs
+++ LibraryEditor/Assets/Script/Mobile/BattleLog/LOGTEXT.cs
@@ -10,7 +10,6 @@ using Cysharp.Threading.Tasks;
 
 public class LOGTEXT : MonoBehaviour
 {
-    //Testです
     //フェードアウトの滑らかさ（デフォルト：1秒間に10コマ）
     static int fadeoutsmoothness = 10;
     static float showTime = 3.0f;
@@ -20,6 +19,7 @@ public class LOGTEXT : MonoBehaviour
     private void Awake()
     {
         thisText = gameObject.GetComponent<TextMeshProUGUI>();
+        //test
     }
     public void SetInfo(string text, float timesec)
     {
@@ -48,4 +48,5 @@ public class LOGTEXT : MonoBehaviour
                 await UniTask.DelayFrame(1);
         }
     }
+    //test
 }
