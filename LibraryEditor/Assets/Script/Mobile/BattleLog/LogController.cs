8ae9583278bd2a026a7d90f7150443c0470350b2
hidenori16 <hidenori@keio.jp>
2021-04-18 16:14:46 +0900

    戦闘ログを追加

---
+++ LibraryEditor/Assets/Script/Mobile/BattleLog/LogController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using static Main;
+using static UsefulMethod;
+using Cysharp.Threading.Tasks;
+
+
+public class LogController : MonoBehaviour
+{
+    public LOGTEXT[] logTexts;
+
+    public void Log(string text, float timesec = 3.0f)
+    {        
+        for (int i = 0; i < logTexts.Length; i++)
+        {
+            if (!logTexts[i].isActive)
+            {
+                logTexts[i].SetInfo(text, timesec);
+                return;
+            }
+        }
+        gameObject.transform.GetChild(logTexts.Length - 1).gameObject.GetComponent<LOGTEXT>().SetInfo(text, timesec);
+    }
+}
