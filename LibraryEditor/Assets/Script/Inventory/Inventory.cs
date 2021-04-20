2c71e71b1909f62af69b6f4519e8ea4fa8482db1
KEI_IdleSystem <appopiss1840@gmail.com>
2021-04-20 18:00:51 +0900

    インベントリのテストを追加しなおしました。

--- LibraryEditor/Assets/Script/Inventory/Inventory.cs
+++ LibraryEditor/Assets/Script/Inventory/Inventory.cs
@@ -6,6 +6,62 @@ namespace IdleLibrary.Inventory
 {
     public class Inventory 
     {
-        
+        private List<Item> items = new List<Item>();
+        private Cal maxNum = new Cal(10);
+        public Inventory()
+        {
+            for (int i = 0; i < maxNum.GetValue(); i++)
+            {
+                items.Add(new Item(-1));
+            }
+        }
+        public Item GetItem(int index)
+        {
+            if (index < 0 || index >= maxNum.GetValue())
+            {
+                Debug.LogError("その場所からgetできません");
+                return new Item(-1);
+            }
+
+            return items[index];
+        }
+        public void SetItem(Item item, int index)
+        {
+            if(index < 0 || index >= maxNum.GetValue())
+            {
+                Debug.LogError("セットできません");
+                return;
+            }
+
+            items[index] = item;
+        }
+        public void SetItemByOrder(Item item)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (!items[i].isSet)
+                {
+                    SetItem(item, i);
+                    return;
+                }
+            }
+
+            Debug.LogError("セットできません");
+            return;
+        }
+        public void SwapItem()
+        {
+
+        }
+    }
+
+    public struct Item
+    {
+        public int id { get; }
+        public bool isSet => id >= 0;
+        public Item(int id)
+        {
+            this.id = id;
+        }
     }
 }
--- LibraryEditor/Assets/Tests/EditMode/Editor/Artifact/Inventory/InventoryTest.cs
+++ LibraryEditor/Assets/Tests/EditMode/Editor/Artifact/Inventory/InventoryTest.cs
@@ -6,100 +6,55 @@ using UnityEngine.TestTools;
 using IdleLibrary.Inventory;
 using System.Linq;
 using IdleLibrary;
+using IdleLibrary.Inventory;
 
 namespace Tests
 {
     public class InventoryTest
     {
         [Test]
-        public void CanCreateItemFromInventoryByOrder()
+        public void CanSetItem()
         {
-            var creates  = Enumerable.Range(0, 100).Select(_ => new CreateItem<ItemTest>(new NullSetItem<ItemTest>())).ToArray();
-            var create = new CreateItemByOrder<ItemTest>(creates, new Cal(10));
-            creates[0].SetItem(new ItemTest(1));
-            creates[1].SetItem(new ItemTest(1));
-            create.SetItem(new ItemTest(2));
-            Assert.IsTrue(creates[2] != null);
-            Assert.IsFalse(creates[2].CanSet);
-            Assert.IsTrue(creates[3].GetItem().id != 2);
+            var inventory = new Inventory();
+            inventory.SetItem(new Item(3), 2);
+            Assert.IsTrue(inventory.GetItem(2).id != -1);
+            Assert.AreEqual(inventory.GetItem(2).id ,3);
         }
+
         [Test]
-        public void CanCreateItemFromInventoryByOrderWithSaveArray()
+        public void CanSetItemByOrder()
         {
-            var saveArray = new Item[100];
-            var creates = Enumerable.Range(0, 100).Select(id => new CreateItem<Item>(new SetItemToSave<Item>(id,saveArray))).ToArray();
-            var create = new CreateItemByOrder<Item>(creates, new Cal(10));
-            creates[0].SetItem(new Item(1));
-            creates[1].SetItem(new Item(1));
-            create.SetItem(new Item(2));
-            Assert.AreEqual(1, saveArray[0].id);
-            Assert.AreEqual(1, saveArray[1].id);
-            Assert.AreEqual(2, saveArray[2].id);
+            var inventory = new Inventory();
+            var item = new Item(1);
+            inventory.SetItem(item, 0);
+            inventory.SetItem(item, 3);
+            inventory.SetItem(item, 4);
+            inventory.SetItem(item, 6);
+            inventory.SetItemByOrder(item);
+            Assert.AreEqual(inventory.GetItem(1).id, 1);
+            inventory.SetItemByOrder(item);
+            Assert.AreEqual(inventory.GetItem(2).id, 1);
+            inventory.SetItemByOrder(item);
+            Assert.AreEqual(inventory.GetItem(5).id, 1);
+            inventory.SetItemByOrder(item);
+            Assert.AreEqual(inventory.GetItem(7).id, 1);
         }
+
         [Test]
         public void CanSwapItem()
         {
-            var items = Enumerable.Range(0, 100).Select(id => new NullSetItem<ItemTest>()).ToArray();
-            items[3].SetItem(new ItemTest(1));
-            items[5].SetItem(new ItemTest(2));
-            //3と5を入れ替えます。
-            var swap = new SwapItem<ItemTest>(items[3]);
-            swap.Stack(items[5]);
-            Assert.AreEqual(1, items[5].GetItem().id);
-            Assert.AreEqual(2, items[3].GetItem().id);
-        }
-        class dummy : IStackItem<ItemTest>, ISetItem<ItemTest>
-        {
-            IStackItem<ItemTest> stack;
-            ISetItem<ItemTest> set;
-            public bool CanSet => true;
-            public dummy(IStackItem<ItemTest> stack , ISetItem<ItemTest> set)
-            {
-                this.stack = stack;
-                this.set = set;
-            }
-            public void Stack(ISetItem<ItemTest> item)
-            {
-                stack.Stack(item);
-            }
 
-            public void SetItem(ItemTest item)
-            {
-                set.SetItem(item);
-            }
-
-            public ItemTest GetItem()
-            {
-                return set.GetItem();
-            }
         }
+
         [Test]
         public void CanSwapItemFromInventory()
         {
-            var set1 = new NullSetItem<ItemTest>();
-            var set2 = new NullSetItem<ItemTest>();
-            //var swap = new SwapItemFromInventory<ItemTest>();
-            var item1 = new dummy(new SwapItem<ItemTest>(set1), set1);
-            var item2 = new dummy(new SwapItem<ItemTest>(set2), set2);
-            item1.SetItem(new ItemTest(1));
-            item2.SetItem(new ItemTest(2));
-            //swap.Click(item1);
-            //これで1が登録された。
-            //swap.Click(item2);
-            //1と2がひっくり返るはず
-            Assert.AreEqual(1, item2.GetItem().id);
-            Assert.AreEqual(2, item1.GetItem().id);
+
         }
         [Test]
         public void CanSwapItemWithNullItem()
         {
-            var items = Enumerable.Range(0, 100).Select(id => new NullSetItem<ItemTest>()).ToArray();
-            items[3].SetItem(new ItemTest(1));
-            //3と5を入れ替えます。ただし、5は何も入ってません。
-            var swap = new SwapItem<ItemTest>(items[3]);
-            swap.Stack(items[5]);
-            Assert.AreEqual(1, items[5].GetItem().id);
-            Assert.AreEqual(0, items[3].GetItem().id);
+
         }
         [Test]
         public void ShouldNotDuplicateWhenSwap()
