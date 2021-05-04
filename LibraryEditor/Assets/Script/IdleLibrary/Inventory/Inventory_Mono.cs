04c0e07e72291319ab4765a289db636dddc293f5
KEI_IdleSystem <appopiss1840@gmail.com>
2021-05-04 19:44:15 +0900

    InventoryInfoを分離しました。これはライブラリでは触りません

--- LibraryEditor/Assets/Script/IdleLibrary/Inventory/Inventory_Mono.cs
+++ LibraryEditor/Assets/Script/IdleLibrary/Inventory/Inventory_Mono.cs
@@ -19,122 +19,9 @@ namespace IdleLibrary
 namespace IdleLibrary.Inventory
 {
 
-	public class InventoryInfo
-    {
-		public Inventory inventory;
-		public Transform canvas;
-		public List<GameObject> items = new List<GameObject>();
-		private GameObject itemPre;
-		private InputItem inputItem;
-		private List<(IInventoryAction action, KeyCode key)> _leftActions = new List<(IInventoryAction action, KeyCode key)>();
-		private List<(IInventoryAction action, KeyCode key)> _rightActions = new List<(IInventoryAction action, KeyCode key)>();
-		private IInventoryAction[] _holdAction = new IInventoryAction[3];
-		public void AddLeftAction(IInventoryAction action, KeyCode key = KeyCode.None)
-		{
-			_leftActions.Add((action,key));
-		}
-		public void AddRightaction(IInventoryAction action, KeyCode key = KeyCode.None)
-        {
-			_rightActions.Add((action, key));
-        }
-		public void RegisterHoldAction(IInventoryAction holdAction, IInventoryAction cancelAction, IInventoryAction releaseAction)
-        {
-			_holdAction[0] = holdAction;
-			_holdAction[1] = cancelAction;
-			_holdAction[2] = releaseAction;
-        }
-		public InventoryInfo(Inventory inventory, Transform canvas, List<GameObject> items, GameObject itemPre, InputItem inputItem)
-        {
-			this.inventory = inventory;
-			this.canvas = canvas;
-			this.items = items;
-			this.itemPre = itemPre;
-			this.inputItem = inputItem;
-			Initialize();
-
-		}
-		void Initialize()
-        {
-			for (int i = 0; i < inventory.GetInventoryLength(); i++)
-			{
-				InstantiateItem();
-			}
-
-			//inventoryLength()の値が変化したら、それに応じてInstantiateする.,
-			this.ObserveEveryValueChanged(_ => inventory.GetInventoryLength()).Subscribe(_ =>
-			{
-				int diff = inventory.GetInventoryLength() - items.Count;
-				if (diff <= 0) return;
-                for (int i = 0; i < diff; i++)
-                {
-					InstantiateItem();
-				}
-			});
-		}
-
-		//Instantiateして、Observableをつける処理をする関数
-		//最終的にはこの中身もカプセル化すべきか？
-		void InstantiateItem()
-        {
-			var item = GameObject.Instantiate(itemPre, canvas);
-			item.GetOrAddComponent<ObservableEventTrigger>().OnPointerDownAsObservable()
-				.Subscribe((UnityEngine.EventSystems.PointerEventData obj) =>
-				{
-					int index = items.IndexOf(item);
-					bool hasKey = false;
-					void DoAction(List<(IInventoryAction action, KeyCode key)> list)
-                    {
-						//keyが登録されているものから探索する。
-						list.Where((pair) => pair.key != KeyCode.None).ToList().ForEach((pair) =>
-						{
-							if (Input.GetKey(pair.key))
-							{
-								hasKey = true;
-								pair.action.Action(index);
-								return;
-							}
-						});
-						if (hasKey) return;
-						list.Where((pair) => pair.key == KeyCode.None).ToList().ForEach((pair) =>
-						{
-							pair.action.Action(index);
-							return;
-						});
-					}
-					if (obj.pointerId == -1)
-					{
-						DoAction(_leftActions);
-					}
-					if (obj.pointerId == -2)
-					{
-						DoAction(_rightActions);
-					}
-				});
-			item.GetOrAddComponent<ObservableEventTrigger>().OnPointerEnterAsObservable()
-				.Subscribe(_ => {
-					inputItem.cursorId = items.IndexOf(item);
-					});
-			item.GetOrAddComponent<ObservableEventTrigger>().OnBeginDragAsObservable()
-				.Subscribe(_ => _holdAction[0].Action(items.IndexOf(item)));
-			item.GetOrAddComponent<ObservableEventTrigger>().OnDropAsObservable()
-				.Subscribe(_ => {
-					if (inputItem.cursorId == -1) 
-					{
-						_holdAction[1].Action(items.IndexOf(item));
-                    }
-                    else
-                    {
-						_holdAction[2].Action(items.IndexOf(item));
-                    }
-						});
-			items.Add(item);
-
-        }
-		
-    }
-
 	public class Inventory_Mono : Subject
 	{
+		/*
 		[SerializeField]
 		Button GenerateItemButton;
 		[SerializeField]
@@ -143,6 +30,7 @@ namespace IdleLibrary.Inventory
 		Button ExpandEquipmentInventory;
 		[SerializeField]
 		Button SortByIdButton;
+		*/
 
 		public GameObject item;
 		public TextMeshProUGUI inventoryItemInfoText;
@@ -176,7 +64,7 @@ namespace IdleLibrary.Inventory
 			//クリックじゃなくてドラッグアンドドロップにもできる
 			var swap = new SwapItem(inventory.inventory, inputItem);
 			inventory.RegisterHoldAction(swap, new Releaseitem(inputItem), swap);
-			inventory.AddLeftAction(new ShowInfoToTextField(inventory.inventory, inventoryItemInfoText));
+			//inventory.AddLeftAction(new ShowInfoToTextField(inventory.inventory, inventoryItemInfoText));
 			inventory.AddLeftAction(new LockItem(inventory.inventory), KeyCode.L);
 			inventory.AddLeftAction(new DeleteItem(inventory.inventory), KeyCode.D);
 			inventory.AddRightaction(new RevertItemToOtherInventory(inventory.inventory,equipmentInventory.inventory));
@@ -190,7 +78,7 @@ namespace IdleLibrary.Inventory
 			//canvas.gameObject.GetOrAddComponent<ObservableEventTrigger>().OnPointerExitAsObservable()
 			//	.Subscribe(_ => inputItem.cursorId = -1);
 
-
+			/*
 			GenerateItemButton.OnClickAsObservable().Subscribe(_ => {
 				inventory.inventory.GenerateItemRandomly();
 				equipmentInventory.inventory.GenerateItemRandomly();
@@ -207,6 +95,7 @@ namespace IdleLibrary.Inventory
 			{
 				inventory.inventory.SortById();
 			});
+			*/
 
 			SaveInventory();
 			Notify();
