cb304853c9c500073a56e2b47c2970ba58d159c9
Appopiss <appopiss1840@gmail.com>
2021-11-04 02:42:48 +0900

    Updateにtrigger追加

--- Library/ResourceDistribution/ElementFactory.cs
+++ Library/ResourceDistribution/ElementFactory.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using System;
+using System.Linq;
 
 namespace IdleLibrary.ProgressSlider.ResourceDistribution
 {
     public class ElementUIFactory<T> : MonoBehaviour where T : MonoBehaviour, IUI
     {
-        public T CreateElement(Element element, T ui, Transform canvas)
+        public T CreateElement(Element element, T ui, Transform canvas, Func<bool> IsUpdateTrigger = null)
         {
+            var trigger = IsUpdateTrigger == null ? () => true : IsUpdateTrigger;
             var instantiatedUi = Instantiate(ui, canvas);
             instantiatedUi.storeButton.onClick.AddListener(() => element.Store());
             instantiatedUi.retrieveButton.onClick.AddListener(() => element.Retrieve());
-            this.ObserveEveryValueChanged(_ => element.CurrentProgressRatio()).Subscribe(_ => instantiatedUi.progressSlider.value = element.CurrentProgressRatio());
+            this.ObserveEveryValueChanged(_ => element.CurrentProgressRatio())
+                .Where(_ => trigger())
+                .Subscribe(_ => instantiatedUi.progressSlider.value = element.CurrentProgressRatio())
+                .AddTo(this);
             Observable.EveryFixedUpdate().Subscribe(_ => element.Update());
             return instantiatedUi;
         }
