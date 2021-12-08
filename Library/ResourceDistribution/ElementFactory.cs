2fb7aa83479740ff1209133d8f33f65361eea84b
Appopiss <appopiss1840@gmail.com>
2021-12-08 10:20:27 +0900

    Elementクラスのライブラリ依存性を削除

--- Library/ResourceDistribution/ElementFactory.cs
+++ Library/ResourceDistribution/ElementFactory.cs
@@ -9,7 +9,7 @@ namespace IdleLibrary.ProgressSlider.ResourceDistribution
 {
     public class ElementUIFactory<T> : MonoBehaviour where T : MonoBehaviour, IUI
     {
-        public T CreateElement(Element element, T ui, Transform canvas, Func<bool> IsUpdateTrigger = null)
+        public T CreateElement(IElement element, T ui, Transform canvas, Func<bool> IsUpdateTrigger = null)
         {
             var trigger = IsUpdateTrigger == null ? () => true : IsUpdateTrigger;
             var instantiatedUi = Instantiate(ui, canvas);
--- Library/ResourceDistribution/ResourceDistribution.cs
+++ Library/ResourceDistribution/ResourceDistribution.cs
@@ -21,66 +21,11 @@ namespace IdleLibrary.ProgressSlider.ResourceDistribution {
         Slider progressSlider { get; }
     }
 
-    //INumberではなく、専用Numberに後ほど書き換える必要あり
-    public class Element : ILevel
+    public interface IElement
     {
-        public long level { get => progressSlider.level; set => progressSlider.level = value; }
-        private readonly INumber number;
-        public double stored;
-        public readonly ProgressSlider progressSlider;
-        private readonly Func<float> ratio = () => 1.0f;
-        public Element(INumber number, ILevel level, Func<double> RequiredProgress, Func<double, double> ProgressSpeedPerFrame, Func<float> ratio)
-        {
-            this.number = number;
-            this.progressSlider = new ProgressSlider(RequiredProgress, () => ProgressSpeedPerFrame(this.stored), level);
-            this.ratio = ratio;
-        }
-        public void Store()
-        {
-            var storing = number.Number * ratio();
-            stored += storing;
-            number.Decrement(storing);
-        }
-        public void Retrieve()
-        {
-            if (CurrentProgressRatio() >= 0.99) progressSlider.currentProgress = 0;
-            number.Increment(stored);
-            stored = 0;
-        }
-        public void Update()
-        {
-            if(progressSlider != null)
-                progressSlider.Update();
-        }
-        public float CurrentProgressRatio()
-        {
-            if (progressSlider != null)
-                return progressSlider.CurrentProgressRatio();
-            else
-                return 0;
-        }
-        public double CurrentProgress() => progressSlider.currentProgress;
-        public float TimeToLevelUp() => progressSlider.TimeToLevelUpForSecond();
-        public static void Load(IEnumerable<Element> elements, double[] loadStored, double[] loadProgress)
-        {
-            elements
-            .Select((element, index) => new { element, index })
-            .ToList()
-            .ForEach(pair => pair.element.stored = loadStored[pair.index]);
-
-            var sliders = elements.Select(element => element.progressSlider);
-            ProgressSlider.Load(sliders, loadProgress);
-        }
-        public static void Save(IEnumerable<Element> elements, double[] loadStored, double[] loadProgress)
-        {
-            elements
-            .Select((element, index) => new { element, index })
-            .ToList()
-            .ForEach(pair => pair.element.ObserveEveryValueChanged(x => x.stored)
-            .Subscribe(_ => loadStored[pair.index] = pair.element.stored));
-
-            var sliders = elements.Select(element => element.progressSlider);
-            ProgressSlider.Save(sliders, loadProgress);
-        }
+        void Store();
+        void Retrieve();
+        float CurrentProgressRatio();
+        void Update();
     }
 }
